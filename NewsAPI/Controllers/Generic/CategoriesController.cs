36a783de3150f03f90817ca66e5bf1566383561f
T0kisaki <Tom161218@gmail.com>
2024-09-26 17:08:26 +0700

    Update Category

---
+++ NewsAPI/Controllers/Generic/CategoriesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NewsAPI.Controllers.Generic
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+    }
+}

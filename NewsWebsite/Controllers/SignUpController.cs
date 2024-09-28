154e1d0d1ab9a028ea68e80c3c9cc1e926d35e6c
KhanhNguyenx <162450705+KhanhNguyenx@users.noreply.github.com>
2024-09-28 21:54:21 +0700

    Create SignUp/SignIn

---
+++ NewsWebsite/Controllers/SignUpController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NewsWebsite.Controllers
+{
+    public class SignUpController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

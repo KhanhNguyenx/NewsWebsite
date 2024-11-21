2d3fe322d9f11b66b2d5681c6a2970cedd465308
TuanNguyen2Year <168094666+TuanNguyen2Year@users.noreply.github.com>
2024-11-21 15:37:23 +0700

    register

--- NewsWebsite/Controllers/HomeController.cs
+++ NewsWebsite/Controllers/HomeController.cs
@@ -133,7 +133,7 @@ namespace NewsWebsite.Controllers
         {
             return View();
         }
-
+      
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
